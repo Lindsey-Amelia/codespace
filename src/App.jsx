8e1b4b571cac46423de3c45e0022b0fa6330137f
Lindsey Amelia <80381290+Lindsey-Amelia@users.noreply.github.com>
2023-08-14 11:46:43 +0000

    set homepage for github deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Lindsey Omoto",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "lindseyomoto46@gmail.com",
+  gitHub: "Lindsey-Amelia",
+  instagram: "Amelia Ofire",
+  linkedIn: "Lindsey Amelia Omoto",
+  medium: "Lindsey Omoto",
+  twitter: "Amelia Lindsey",
+  youTube: "Lindsey A. Omoto",
 };
 
 const primaryColor = "#4E567E";
